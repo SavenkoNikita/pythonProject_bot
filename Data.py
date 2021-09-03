a06b823003e0785f7d8d05651d867eaa44e79579
Nikita <SavenkoNikita@yandex.com>
2021-09-03 09:06:51 +0300

    download data

---
+++ Data.py
@@ -0,0 +1,35 @@
+import telebot
+
+TOKEN = '1013175932:AAGIDc3GACkwXPGK8XRLfJkNQhO_J4xtflw'  # токен бота
+bot = telebot.TeleBot(TOKEN)
+
+# Словарь администраторов
+list_admins = {
+    'Никита': 1827221970,
+    'Дима': 463376780,
+    'Паша': 762058672,
+    'Лёха': 1350324635
+}
+
+# Словарь групп
+list_groups = {
+    'IT_info': -1001332331366,
+    'GateKeepers': -1001450700256
+}
+
+# Путь к файлу 'Графики дежурств'
+host_name = 'R_777'
+pass_log_in = '109225081'
+way = '192.168.0.236/public_files/14_Отдел_ИТ/1_Личные_папки/Савенко_Никита/Документы/Графики дежурств.xlsx'
+route = 'smb://' + host_name + ':' + pass_log_in + '@' + way
+
+# Листы в файле
+sheets_file = {
+    'Дежурный': 'Субботний дежурный(Python)',
+    'Инвертаризация': 'Инвентаризация(Python)',
+    'Уведомления': 'Уведомления(Python)'
+}
+
+# Путь к базе данных
+way_sql = '/home/nik/Рабочий стол/Проекты python/Data_users_telegram_bot.db'
+
