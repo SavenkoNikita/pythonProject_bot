4173ad7c6544bd1439d324f4fb3071aead66a9ef
Nikita <SavenkoNikita@yandex.com>
2021-11-03 21:13:15 +0300

    Убрал лишние строки в Notifications.py которые мешали отправке сообщений вовремя. И мелкие исправления

--- Notifications.py
+++ Notifications.py
@@ -1,7 +1,6 @@
 import sqlite3
 import time
 
-import Clear_old_data
 import Data
 import Other_function
 import Read_file
@@ -32,45 +31,34 @@ def notifications(sheet_name):
 
 
 # Функция для уведомления всех пользователей находящихся в БД
-def notification_all_reg(text_message, sheet_name):
-    read_type = Read_file.read_file(sheet_name)['Type']
-    difference_date = Read_file.read_file(sheet_name)['Dif date']
+def notification_all_reg(text_message):
+    try:
+        sqlite_connection = sqlite3.connect(Data.way_sql)
+        cursor = sqlite_connection.cursor()
+        # print('Подключен к SQLite')
 
-    if read_type == 'date':
-        if difference_date < 0:  # Если событие в прошлом
-            Clear_old_data.clear(sheet_name)  # Очистить старые данные
-            notification_all_reg(text_message, sheet_name)  # Перезапустить функцию
-        elif difference_date == 0:
-            try:
-                sqlite_connection = sqlite3.connect(Data.way_sql)
-                cursor = sqlite_connection.cursor()
-                print('Подключен к SQLite')
+        sqlite_select_query = 'SELECT * from users where user_id'
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchall()
+        # print('ID всех пользователей:\n')
+        all_user_sql = []
+        for row in records:
+            all_user_sql.append(row[1])
+        cursor.close()
 
-                sqlite_select_query = 'SELECT * from users where user_id'
-                cursor.execute(sqlite_select_query)
-                records = cursor.fetchall()
-                print('ID всех пользователей:\n')
-                all_user_sql = []
-                for row in records:
-                    all_user_sql.append(row[1])
-                cursor.close()
+    except sqlite3.Error as error:
+        print('Ошибка при работе с SQLite', error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print('Соединение с SQLite закрыто')
 
-            except sqlite3.Error as error:
-                print('Ошибка при работе с SQLite', error)
-            finally:
-                if sqlite_connection:
-                    sqlite_connection.close()
-                    print('Соединение с SQLite закрыто')
-
-            i = 0
-            while i < len(all_user_sql):
-                print(all_user_sql[i])
-                Data.bot.send_message(chat_id=all_user_sql[i], text=text_message)
-                time.sleep(1)
-                i += 1
-    else:
-        print('В ' + sheet_name + ' нет данных или не подходящий формат.')
-        print('Ошибку выдал notification_all_reg')
+    i = 0
+    while i < len(all_user_sql):
+        print(all_user_sql[i])
+        Data.bot.send_message(chat_id=all_user_sql[i], text=text_message)
+        time.sleep(1)
+        i += 1
 
 
 # Уведомления для юзеров с указанными параметрами
@@ -78,18 +66,19 @@ def notification_for(text_message, column, column_meaning):
     try:
         sqlite_connection = sqlite3.connect(Data.way_sql)
         cursor = sqlite_connection.cursor()
-        print('Подключен к SQLite')
+        # print('Подключен к SQLite')
 
         sqlite_select_query = 'SELECT * FROM users WHERE ' + column + ' = ?'
         cursor.execute(sqlite_select_query, [column_meaning])
         records = cursor.fetchall()
-        print('Список ID:\n')
+        # print('Список ID:\n')
         all_id_sql = []
         for row in records:
             all_id_sql.append(row[1])
         cursor.close()
-        print(all_id_sql)
+        # print(all_id_sql)
         i = 0
+        print('Уведомления отправлены следующим пользователям:\n')
         while i < len(all_id_sql):
             print(all_id_sql[i])
             Data.bot.send_message(chat_id=all_id_sql[i], text=text_message)
@@ -108,22 +97,22 @@ def send_sticker_for(user_first_name, column, column_meaning):
         sticker = SQLite.get_user_sticker(Other_function.get_key(Data.user_data, user_first_name))
         sqlite_connection = sqlite3.connect(Data.way_sql)
         cursor = sqlite_connection.cursor()
-        print('Подключен к SQLite')
+        # print('Подключен к SQLite')
 
         sqlite_select_query = 'SELECT * FROM users WHERE ' + column + ' = ?'
         cursor.execute(sqlite_select_query, [column_meaning])
         records = cursor.fetchall()
-        print('Список ID:\n')
+        # print('Список ID:\n')
         all_id_sql = []
         for row in records:
             all_id_sql.append(row[1])
         cursor.close()
-        print(all_id_sql)
+        # print(all_id_sql)
         i = 0
+        print('Стикер отправлен следующим пользователям:\n')
         while i < len(all_id_sql):
             print(all_id_sql[i])
             Data.bot.send_sticker(all_id_sql[i], sticker)
-            # Data.bot.send_sticker(chat_id=all_id_sql[i], SQLite.get_user_sticker(Other_function.get_key(Data.user_data, sticker_in_user_id)))
             i += 1
     except sqlite3.Error as error:
         print("Ошибка при работе с SQLite", error)
