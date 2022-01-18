bf1e4189660de853582d9da5c30595dad7d55c7e
Nikita <SavenkoNikita@yandex.com>
2022-01-18 08:47:37 +0300

    Сделал вывод на экран более информативным

--- Notifications.py
+++ Notifications.py
@@ -10,23 +10,28 @@ def notification_all_reg(text_message):
     sqlite_connection = sqlite3.connect(Data.way_sql)
     try:
         cursor = sqlite_connection.cursor()
-        # print('Подключен к SQLite')
 
         sqlite_select_query = 'SELECT * from users where user_id'
         cursor.execute(sqlite_select_query)
         records = cursor.fetchall()
-        # print('ID всех пользователей:\n')
         all_user_sql = []
         for row in records:
             all_user_sql.append(row[1])
         cursor.close()
         i = 0
+        print('Уведомления отправлены следующим пользователям:\n')
         while i < len(all_user_sql):
-            print(all_user_sql[i])
-            Data.bot.send_message(chat_id=all_user_sql[i], text=text_message)
-            time.sleep(1)
+            username = SQLite.get_user_info(all_user_sql[i])
+            try:
+                print(username)
+                Data.bot.send_message(all_user_sql[i], text=text_message)
+                # Data.bot.send_message(Data.list_admins.get('Никита'), text=text_message)
+            except Data.telebot.apihelper.ApiTelegramException:
+                text_error = 'Пользователь <' + username + '> заблокировал бота!'
+                print(text_error)
+                Other_function.logging_event('error', str(text_error))
+                SQLite.log_out(all_user_sql[i])
             i += 1
-
     except sqlite3.Error as error:
         print('Ошибка при работе с SQLite', error)
         Other_function.logging_event('error', error)
