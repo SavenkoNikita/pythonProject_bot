46546436122d8aca204cb68e93e80f772a4cd7e6
Nikita <SavenkoNikita@yandex.com>
2022-03-08 21:56:52 +0300

    мелкие изменения

--- technical works.py
+++ technical works.py
@@ -8,8 +8,17 @@ bot = Data.bot
 @bot.message_handler(content_types=['text'])
 def other_functions(message):
     technical_timeout = 'Ведутся технические работы. Бот временно недоступен :('
-    bot.reply_to(message, technical_timeout)
+    mes_id = bot.reply_to(message, technical_timeout)
     print(f'Пользователь {message.from_user.first_name} написал:\n{message.text}')
+    print(message.message_id)
+    # print(message.message_id + 1)
+    time.sleep(3)
+    bot.delete_message(message.from_user.id, message_id=mes_id.message_id)
+
+    # for id in range((message.message_id - 5), message.message_id):
+    #     bot.delete_message(chat_id=1013175932, message_id=id)
+
+    bot.send_poll(id, 'вопрос', options=['1', '2', '3'], )
 
 
 if __name__ == '__main__':
