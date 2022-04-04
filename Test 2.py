b9a8f328d3db8cb4f7112ef60e71de3fe37c233d
Nikita <SavenkoNikita@yandex.com>
2022-04-04 14:43:37 +0300

    Добавил в исключения файлы с тестами

---
+++ Test 2.py
@@ -0,0 +1,31 @@
+# import Functions
+#
+# Functions.File_processing('Дежурный').check_dej_tomorrow()
+
+import Functions
+# import Data
+#
+# message_id = Data.bot.send_message(1827221970, 'Start tracking sensor in defroster').message_id
+# message_id = 35181
+# # message_id = int(message_id)
+# print(message_id)
+# print(type(message_id))
+#
+# Functions.SQL().update_mess_id_by_table_def(1827221970, message_id)
+
+# print(Functions.SQL().get_dict())
+
+# Functions.Notification().update_mess()
+# import Tracking_sensor
+
+# Functions.Notification().update_mess('observers_for_faulty_sensors', Tracking_sensor.TrackingSensor().check_all())
+# Functions.Notification().update_mess('tracking_sensor_defroster', Tracking_sensor.TrackingSensor().check_defroster())
+
+# Functions.SQL().update_mess_id_by_table(1827221970, message_id, 'observers_for_faulty_sensors', 'observer_all_sensor')
+
+# Functions.File_processing('Уведомления для всех').check_event_today()
+# Functions.File_processing('Уведомления для подписчиков').check_event_today()
+# Functions.File_processing('Уведомления для админов').check_event_today()
+
+# Functions.File_processing('Дежурный').check_dej_tomorrow()
+
--- Test.py
+++ Test.py
@@ -1,90 +1,40 @@
-# import Data
-# import Other_function
-#
-# for i in Data.sheets_file:
-#     print(i)
-#     print(f'{Other_function.File_processing(i).read_file()}\n\n')
-# import time
-#
-# import Data
-# import Other_function
-# import Tracking_sensor
-# from Data import bot
-
-# test_list = ['Тестовое сообщение',
-#              'Тестовое сообщение2',
-#              'Тестовое сообщение3'
-#              ]
-# for i in test_list:
-#     bot.send_message(chat_id=Data.list_admins.get('Никита'), text=i)
-#     time.sleep(5)
-#     bot.delete_message(chat_id=Data.list_admins.get('Никита'), message_id=bot.send_message(chat_id=Data.list_admins.get('Никита'), text=i))
-
-# bot.delete_message(chat_id=Data.list_admins.get('Никита'), message_id=1)
-# list_id = [
-#     Data.list_admins.get('Никита'),
-#     368861606
-# ]
-#
-# for id in list_id:
-#     bot.send_poll(id, 'вопрос', options=['1', '2', '3'],)
-
-# print(Other_function.File_processing('Уведомления для подписчиков').check_event_today())
-# print(Other_function.File_processing('Дежурный').read_file())
-# print(Other_function.File_processing('Дежурный').next_dej())
-# import datetime
-
-# import Functions
-# import Other_function
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
 
 
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
 
-# text_message = 0
-# id_mes = bot.send_message(Data.list_admins.get('Никита'), text_message)
-# bot.pin_chat_message(chat_id=Data.list_admins.get('Никита'), message_id=id_mes.message_id)
-# while text_message < 10:
-#     text_message += 1
-#     bot.edit_message_text(text=text_message, chat_id=Data.list_admins.get('Никита'), message_id=id_mes.message_id)
-#     time.sleep(5)
 
-# my_dict = {'test': 1}
-# print(my_dict)
-# my_dict['test2'] = 2
-# print(my_dict)
+class Lecturer(Mentor):
 
-# print(Tracking_sensor.TrackingSensor().check_defroster())
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
-# print(Other_function.File_processing('Уведомления для подписчиков').create_event('18.03.2022', 'test'))
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
 
-# date = '18.03.2022'
-# date_obj = datetime.datetime.strptime(date, '%d.%m.%Y')
-# date_obj = date_obj.date().strftime('%d.%m.%Y')
-# print(date_obj)
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
 
-# Other_function.File_processing('Дежурный').check_dej_tomorrow()
-# import Scheduler
-#
-# Scheduler.repeat_for_list()
-import Functions
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
 
-# data_list = Functions.File_processing('Уведомления для подписчиков').read_file()
-#
-# if data_list is not None:
-#     for i in data_list:
-#         if len(i) == 2:
-#             print(f'Первая дата: {type(i[0])}({i[0]})\nТекст: {type(i[1])}({i[1]})\n')
-#             print(i)
-#             print(f'Кол-во значений: {len(i)}\n\n')
-#         elif len(i) == 3:
-#             print(f'Первая дата: {type(i[0])}({i[0]})\nВторая дата: {type(i[1])}({i[1]})\nТекст: {type(i[2])}({i[2]})\n')
-#             print(i)
-#             print(f'Кол-во значений: {len(i)}\n\n')
-
-# Functions.File_processing('Уведомления для подписчиков').clear_old_data()
-
-# if -13 <= -999.9:
-#     print(True)
-# else:
-#     print(False)
-
-print(Functions.File_processing('Дежурный').next_dej())
+print(best_student.grades)
